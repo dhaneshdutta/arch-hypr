6bf7f084597d7a4a4651048174d1d4178dae651c
dhanesh <152627573+dhaneshdutta@users.noreply.github.com>
2024-05-05 17:35:38 +0530

    Update pacman.sh

--- install-scripts/pacman.sh
+++ install-scripts/pacman.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 💫 https://github.com/JaKooLit 💫 #
+# 💫 https://github.com/dhaneshdutta 💫 #
 # pacman adding up extra-spices #
 
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
