a7a2290527716bbbd941e1e950cce4056772f7a5
dhanesh <152627573+dhaneshdutta@users.noreply.github.com>
2024-05-05 17:39:54 +0530

    Update zsh.sh

--- install-scripts/zsh.sh
+++ install-scripts/zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 💫 https://github.com/JaKooLit 💫 #
+# 💫 https://github.com/dhaneshdutta 💫 #
 # zsh and oh my zsh including pokemon-color-scripts#
 if [[ $USE_PRESET = [Yy] ]]; then
   source ./preset.sh
